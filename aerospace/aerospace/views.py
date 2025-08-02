7626a06c3e1e0819cf6b50b190c7f20e95a88e77
Xiredge <jerick.gamba@outlook.ph>
2025-08-02 18:25:58 +0800

    Added .gitignore

--- aerospace/aerospace/views.py
+++ aerospace/aerospace/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+import asyncio
+from .get_json import fetch_data
+from .data_storage import *
 
 def homepage(request):
-    return HttpResponse("Hello world!")
+    data = asyncio.run(fetch_data("https://www.simcompanies.com/api/v2/companies/buildings/38184904/sales-orders/"))
+    store_data(data, "json.json")
+    info = read_data("json.json")
+    return HttpResponse(info)
