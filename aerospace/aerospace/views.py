52f3b9d60c26893407b177f6dc6c3e445b2e357f
Xiredge <jerick.gamba@outlook.ph>
2025-08-03 18:37:40 +0800

    Added get_data and returns its dict, also add total net and gross profit that we also return

--- aerospace/aerospace/views.py
+++ aerospace/aerospace/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 import asyncio
 from .get_json import fetch_data
 from .data_storage import *
@@ -9,11 +10,22 @@ from .read_chat import read_chat
 
 def homepage(request):
     data = asyncio.run(fetch_data("https://www.simcompanies.com/api/v2/companies/buildings/38184904/sales-orders/"))
+    data += asyncio.run(fetch_data("https://www.simcompanies.com/api/v2/companies/buildings/38321052/sales-orders/"))
+    data += asyncio.run(fetch_data("https://www.simcompanies.com/api/v2/companies/buildings/38315475/sales-orders/"))
     store_data(data, "json.json")
     info = read_data("json.json")
+    sales_data = get_data({}, info)
+    total_gross_income = 0
+    total_net_income = 0
 
-    products = {}
-
-    get_data(products, read_data("json.json"))
-    get_summary(products)
-    return HttpResponse(info)
+    for item in sales_data:
+        if "net_income" and "gross_profit" in item:
+            total_net_income += item["net_income"]
+            total_gross_income += item["gross_profit"]
+ 
+    #get_summary(products)
+    return render(request, "home.html", {
+        "products": sales_data, 
+        "total_net_income": total_net_income,
+        "total_gross_income": total_gross_income,
+        })
